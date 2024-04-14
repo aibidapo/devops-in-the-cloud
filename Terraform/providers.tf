3cf1c5b501c29689fc0b53203fc013bebfdb9756
aibidapo <aibidapo@gmail.com>
2024-04-14 07:36:18 -0400

    Modified the providers.tf file to ensure the appropriate profile is used

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  # shared_credentials_files = "/home/ansible/.aws/credentials"
+  shared_credentials_files = ["/home/ansible/.aws/credentials"]
   # profile                  = "iamadmin-prod"
 
 }
