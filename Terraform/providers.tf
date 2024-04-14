8e015bd6af45259ed1b5cf201d493ae7d9e636ac
Ubuntu <ubuntu@ip-172-31-16-76.ec2.internal>
2024-04-14 19:43:27 +0000

    Downgrading the version of terraform to address errors

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.74.2"
+      version = "5.42.0"
     }
   }
 }
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_file = "~/.aws/credentials"
+  shared_credentials_files = ["~/.aws/credentials"]
   # profile                  = "iamadmin-prod"
 
 }
